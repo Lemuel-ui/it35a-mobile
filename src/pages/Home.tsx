547587054c2d0b7979e5b39fb2018976248313b2
joshua762002 <obradorlemuel57@gmail.com>
2026-04-13 08:53:21 +0800

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonToolbar } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router"
+import { Route, Redirect } from "react-router";
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
+    <IonReactRouter>
+      <IonTabs>
+      <IonTabBar slot="bottom">
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTittle>Tabs</IonTittle>
         </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+        {/**/}
+        {tab.map((item,index) =>(
+          <IonTabButton key={index} tab={item.tab} href={item.url}>
+            <Ionicon icon={item.icon} />
+            <IonLabel>{item.name}</IonLabel>
+          </IonTabButton>
+
+        ))}
+        </IonTabBar>
+<IonRouterOutlet>
+  <Route exact path="/app/home/feed" compnent={Feed} />
+  <Route exact path="/app/home">
+      <Redirect to="/app/home/feed"/>
+  </Route>
+  <Route exact path="/app/home/search" component={Search} />
+  <Route exact path="/app/home/favorites" component={Favorites} />
+</IonRouterOutlet>
+</IonTabs>
+</IonReactRouter>
   );
 };
-
-export default Home;
